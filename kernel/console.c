60fc7a6b458d52959ba6489b635525723ec260bf
Baldi <d.baldischwieler@t-online.de>
2024-10-24 14:28:35 +0200

    [FEAT] Adding clear hotkey (STRG + L)

--- kernel/console.c
+++ kernel/console.c
@@ -155,6 +155,10 @@ consoleintr(int c)
       consputc(BACKSPACE);
     }
     break;
+   case C('L'):
+     printf("\e[1;1H\e[2J");
+     printf("$ ");
+     break;
   default:
     if(c != 0 && cons.e-cons.r < INPUT_BUF_SIZE){
       c = (c == '\r') ? '\n' : c;
